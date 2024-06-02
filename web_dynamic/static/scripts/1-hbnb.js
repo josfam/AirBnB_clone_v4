61c62391392415e419a0373cd920ac3bd9a04c62
josfam <jamukun@proton.me>
2024-06-02 09:49:47 +0300

    Fix: Move script link to the head of the template
    The javascript code has also been updated to use JQuery's
    `$(document).ready(...)` functionality

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,26 @@
 #!/usr/bin/node
 
-const amenitiesShown = $('.amenities h4');
-const checkboxes = $('.amenities input[type="checkbox"]');
-const amenities = {};
+$(document).ready(function () {
+  const amenitiesShown = $('.amenities h4');
+  const checkboxes = $('.amenities input[type="checkbox"]');
+  const amenities = {};
 
-const addOrRemoveAmenity = function () {
-  const checkbox = $(this);
-  const amenityId = checkbox.attr('data-id');
-  const amenityName = checkbox.attr('data-name');
-  if (checkbox.is(':checked')) {
-    amenities[amenityId] = amenityName;
-  } else {
-    delete amenities[amenityId];
-  }
-  const amenitiesString = Object.values(amenities).join(', ');
-  amenitiesShown.text(amenitiesString);
-};
+  /* Adds/removes an amenity name to/from a comma-separated list shown directly
+  under the `Amenities`, depending on whether the amenity is checked/unchecked
+  from the drop-down menu.
+  */
+  const addOrRemoveAmenity = function () {
+    const checkbox = $(this);
+    const amenityId = checkbox.attr('data-id');
+    const amenityName = checkbox.attr('data-name');
+    if (checkbox.is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+    const amenitiesString = Object.values(amenities).join(', ');
+    amenitiesShown.text(amenitiesString);
+  };
 
-checkboxes.on('change', addOrRemoveAmenity);
+  checkboxes.on('change', addOrRemoveAmenity);
+});
